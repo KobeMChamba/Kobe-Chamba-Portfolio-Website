3b41a65406f0381195f5125f214ea7389175b248
Kobe Chamba <77932095+KobeMChamba@users.noreply.github.com>
2024-07-03 18:41:05 -0500

    Completed Hero Component

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/289WXTD2-3KT8.jpg";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import githubLight from "../../assets/github-light.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import CV from "../../assets/Kobe_Chamba_Resume.pdf";
+import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
-  return <div>Hero</div>;
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={heroImg}
+          alt="Profile Picture of Kobe Chamba"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Kobe
+          <br />
+          Chamba
+        </h1>
+        <h2> Fullstack Developer</h2>
+        <span>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter Icon" />
+          </a>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github Icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin Icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
