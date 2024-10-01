9624cd8dd5d9053553a63d92809a18efdf8c868b
Kobe Chamba <77932095+KobeMChamba@users.noreply.github.com>
2024-10-01 15:54:27 -0400

    Added Chess Openings Calculator Project Card

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./ProjectsStyles.module.css";
 import NUwebdev from "../../assets/NUWebdev.jpeg";
 import HandTrackingLogo from "../../assets/AI HandTracking Logo.jpeg"
+import ChessLogo from "../../assets/Chess Openings Calculator Logo.jpeg"
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -10,18 +11,26 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
       <ProjectCard
-          src={HandTrackingLogo}
-          link="https://github.com/KobeMChamba/Python-Projects/tree/main/Hand%20Tracking%20Project"
-          h3={"AI Hand Tracking"}
-          p3={"I developed a hand-tracking app using machine learning libraries such as OpenCV and Mediapipe. This project marked my first experience with these libraries, allowing me to dive into computer vision and real-time hand tracking."}
-        />
+        src={ChessLogo}
+        link="https://github.com/KobeMChamba/Python-Projects/tree/main/Openings%20Calculator"
+        h3={"Chess Openings Calculator"}
+        p3={"A Python project utilizing the Chess.com API to analyze and gain insights into the openings of my chess games. By fetching game data, I was able to track patterns, evaluate the effectiveness of different opening strategies, and identify areas for improvement."}
+      />
+      
+      <ProjectCard
+        src={HandTrackingLogo}
+        link="https://github.com/KobeMChamba/Python-Projects/tree/main/Hand%20Tracking%20Project"
+        h3={"AI Hand Tracking"}
+        p3={"I developed a hand-tracking app using machine learning libraries such as OpenCV and Mediapipe. This project marked my first experience with these libraries, allowing me to dive into computer vision."}
+      />
         
-        <ProjectCard
-          src={NUwebdev}
-          link="https://github.com/KobeMChamba/webdev-labs/tree/main"
-          h3={"NU Webdev"}
-          p3={"In my first web development class at Northwestern University, I learned the fundamentals of web development, including HTML, CSS, and JavaScript. Through hands-on projects, I built foundational skills in creating responsive web pages and gained a solid understanding of front-end design and functionality. These early projects helped me develop a passion for web development and provided a strong base for more advanced work."}
-        />
+      <ProjectCard
+        src={NUwebdev}
+        link="https://github.com/KobeMChamba/webdev-labs/tree/main"
+        h3={"NU Webdev"}
+        p3={"In my first web development class at Northwestern University, I learned the fundamentals of web development, including HTML, CSS, and JavaScript."}
+      />
+
         {/* <ProjectCard
           src={viberr}
           link="https://github.com/KobeMChamba/webdev-labs/tree/main"
